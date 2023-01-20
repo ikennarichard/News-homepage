65b5926bc1c6843781bcec1a663dfa96d118257e
Ikenna Richard <oguejioforrichard@gmail.com>
2023-01-20 12:42:16 -1100

    Refactor code

---
+++ script.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('.hamburger_icon');
+
+const nav = document.querySelector('nav');
+
+const leftLayer = document.querySelector('.left_layer')
+
+menu.addEventListener('click', (e) => {
+    e.target.classList.toggle('close');
+
+    nav.classList.toggle('show_nav');
+    leftLayer.classList.toggle('show_layer')
+})
